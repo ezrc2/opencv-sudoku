8d2942df5d96c637d95c6f9e14b8b16e84f1cecf
Eric Chen <ezchen2556@gmail.com>
2021-01-31 20:51:48 -0800

    Find contours and keep the one with largest area

--- image_processing.py
+++ image_processing.py
@@ -7,9 +7,19 @@ while True:
     frame = cv2.flip(frame, 1)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(frame, (7, 7), 3)
+    blur = cv2.GaussianBlur(gray, (7, 7), 3)
     thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     thresh = cv2.bitwise_not(thresh)
+
+    contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    sudoku_outline = None
+    for contour in contours:
+        perimeter = cv2.arcLength(contour, True)
+        shape = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+        if len(shape) == 4:
+            sudoku_outline = shape
+            break
 
     cv2.imshow("frame", frame)
     cv2.imshow("thresh", thresh)
