78b06fc8204ce353472c1e99fac3024428b984ce
Eric Chen <ezchen2556@gmail.com>
2021-03-11 20:52:08 -0800

    Add tensorflow mnist convolutional neural network

---
+++ CNN.py
@@ -0,0 +1,22 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(60000, 28, 28, 1) # grayscale
+X_test = X_test.reshape(10000, 28, 28, 1)
+
+model = Sequential()
+model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # 10 classes
+
+model.fit(X_train, to_categorical(y_train), epochs=3, validation_data=(X_test, to_categorical(y_test)))
+
+model.save_weights('model.h5')
