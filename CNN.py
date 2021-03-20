63ebd3597cac1bbf47340cb3a298ef88e13cad97
Eric Chen <ezchen2556@gmail.com>
2021-03-19 22:10:16 -0700

    Add max pooling layers

--- CNN.py
+++ CNN.py
@@ -1,23 +1,31 @@
 import numpy as np
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, Dense, Flatten, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-X_train = X_train.reshape(60000, 28, 28, 1) # grayscale
-X_test = X_test.reshape(10000, 28, 28, 1)
+# X_train = X_train.reshape(60000, 28, 28, 1) # grayscale
+# X_test = X_test.reshape(10000, 28, 28, 1)
+X_train = np.expand_dims(X_train, -1)
+X_test = np.expand_dims(X_test, -1)
+
+X_train = X_train.astype("float32") / 255.0 # normalize
+X_test = X_test.astype("float32") / 255.0
 
 model = Sequential()
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28, 28, 1)))
-model.add(Conv2D(32, kernel_size=3, activation='relu'))
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
-model.add(Dropout(0.5))
+model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
 model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # 10 classes
 
-model.fit(X_train, to_categorical(y_train), epochs=3, validation_data=(X_test, to_categorical(y_test)))
+model.fit(X_train, to_categorical(y_train, 10), batch_size=128, epochs=15, validation_data=(X_test, to_categorical(y_test)))
 
 model.save('model.h5')
