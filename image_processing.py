c0399c22a68bcc32e82bab2171873fd450ea9ac2
Eric Chen <ezchen2556@gmail.com>
2021-03-20 21:54:21 -0700

    Add documentation

--- image_processing.py
+++ image_processing.py
@@ -10,6 +10,17 @@ from tensorflow.keras.models import load_model
 # the tutorial on how to find the sudoku puzzle using OpenCV
 
 def find_puzzle_outline(image):
+    """Finds the contours of the sudoku puzzle and applies four-point perspective
+        transform to get a straight-on view
+
+    Args:
+        image (numpy.ndarray): The input image
+
+    Returns:
+        puzzle (numpy.ndarray): A cropped and straightened image of the sudoku puzzle
+        gray (numpy.ndarray): A grayscale image of puzzle
+        output (numpy.ndarray): An image of puzzle with the sudoku contour drawn on it
+    """
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (7, 7), 3)
     thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
@@ -38,19 +49,28 @@ def find_puzzle_outline(image):
     cv2.drawContours(output, [puzzle_outline], -1, (0, 255, 0), 2)
     
     puzzle = four_point_transform(image, puzzle_outline.reshape(4, 2))
-    warped = four_point_transform(gray, puzzle_outline.reshape(4, 2))
+    gray = four_point_transform(gray, puzzle_outline.reshape(4, 2))
 
     # cv2.imshow("puzzle", puzzle)
     # cv2.waitKey(0)
     
-    return (puzzle, warped, output)
+    return puzzle, gray, output
 
-def get_digits(warped):
+def get_digits(gray):
+    """Extracts the digits of the Sudoku puzzle and puts it into a 2D numpy array
+
+    Args:
+        gray (numpy.ndarray): A grayscale sudoku image
+
+    Returns:
+        board (numpy.ndarray): A 2D array with the Sudoku digits
+        cell_locations (list): A list of each cell's coordinates on the sudoku image
+    """
     model = load_model("model.h5")
     board_size = 9
-    board = -1 * np.ones((board_size, board_size), dtype="int")
-    dx = warped.shape[1] // board_size
-    dy = warped.shape[0] // board_size
+    board = -1 * np.ones((board_size, board_size), dtype="int") # -1 is empty
+    dx = gray.shape[1] // board_size
+    dy = gray.shape[0] // board_size
     cell_locations = []
 
     for y in range(board_size):
@@ -62,8 +82,8 @@ def get_digits(warped):
             y2 = (y + 1) * dy
             cell_row.append((x1, y1, x2, y2))
 
-            cell = warped[y1:y2, x1:x2]
-            digit = extract_digit_image(cell)
+            cell = gray[y1:y2, x1:x2]
+            digit = extract_cell_image(cell)
 
             if digit is not None:
                 # cv2.imshow("digit", digit)
@@ -78,7 +98,15 @@ def get_digits(warped):
     
     return board, cell_locations
 
-def extract_digit_image(cell):
+def extract_cell_image(cell):
+    """Applies a mask to the digit image in a cell
+
+    Args:
+        cell (numpy.ndarray): An image of a single cell on the sudoku image
+
+    Returns:
+        digit (numpy.ndarray): the pre-processed cell image
+    """
     thresh = cv2.threshold(cell, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     thresh = clear_border(thresh)
     contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -100,6 +128,13 @@ def extract_digit_image(cell):
     return digit
 
 def display_solution(board, puzzle, cell_locations):
+    """Displays the numbers of the solved puzzle onto the sudoku image
+
+    Args:
+        board (numpy.ndarray): A 2D array with the Sudoku digits
+        puzzle (numpy.ndarray): Image of the puzzle
+        cell_locations (list): A list of each cell's coordinates on the sudoku image
+    """
     original = board.copy()
     solver = Sudoku(board)
     solver.solve()
@@ -120,16 +155,19 @@ def display_solution(board, puzzle, cell_locations):
 
 
 def main():
+    """The main loop of the webcam"""
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read() 
         #frame = cv2.flip(frame, 1)
-        puzzle, warped, output = find_puzzle_outline(frame)
+        puzzle, gray, output = find_puzzle_outline(frame)
         if output is not None:
             cv2.imshow("Camera", output)
 
         if cv2.waitKey(1) >= 0:
-            board, cell_locations = get_digits(warped)
+            board, cell_locations = get_digits(gray)
+            print(type(board))
+            print(type(cell_locations))
             display_solution(board, puzzle, cell_locations)
             break
     
